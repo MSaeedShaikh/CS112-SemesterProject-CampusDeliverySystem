cbb4774048f1953c6d36fe83387e627eedadfaa0
MSaeedShaikh <124174667+MSaeedShaikh@users.noreply.github.com>
2023-05-14 18:47:18 +0500

    tiny change

--- head1.h
+++ head1.h
@@ -6,31 +6,31 @@ int BId=0;
 
 class Catalog{
     public:
-    vector<string> name;
-    vector<float> price;
+    vector<string> iname;
+    vector<float> iprice;
     
     int findIndex(string item) {
-        for (auto i = 0; i < name.size(); ++i) {
-            if (name[i] == item)
+        for (int i = 0; i < iname.size(); ++i) {
+            if (iname[i] == item)
                 return i;
         }
         return -1;
     }
     void AddItem(string n, float p){
-        name.push_back(n);
-        price.push_back(p);
+        iname.push_back(n);
+        iprice.push_back(p);
     }
     void RemoveItem(string item){
         int ind = findIndex(item);
         if(ind!=-1){
-            name.erase(name.begin()+ind);
-            price.erase(price.begin()+ind);
+            iname.erase(iname.begin()+ind);
+            iprice.erase(iprice.begin()+ind);
         }
     }
     void ChangePrice(string item, float p){
         int ind = findIndex(item);
         if(ind!=-1){
-            price[ind]=p;
+            iprice[ind]=p;
         }
     }
 };
---
+++ main.cpp
@@ -0,0 +1,6 @@
+#include<iostream>
+#include"head1.h"
+using namespace std;
+
+int main(){
+}
